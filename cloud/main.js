9c57fc23837e56a4d3f7888d2bd55618ae885dbf
zerozaki0752 <albert@AlberttekiMacBook-Pro.local>
2013-11-28 16:14:50 +0800

    1

--- cloud/main.js
+++ cloud/main.js
@@ -32,12 +32,12 @@ AV.Cloud.setInterval('refreash_market', 10, function(){
                     market.save(null, {
                         success: function(gameScore) {
                             // Execute any logic that should take place after the object is saved.
-                            alert('New object created with objectId: ' + gameScore.id);
+                            console.log('New object created with objectId: ' + gameScore.id);
                         },
                         error: function(gameScore, error) {
                             // Execute any logic that should take place if the save fails.
                             // error is a AV.Error with an error code and description.
-                            alert('Failed to create new object, with error code: ' + error.description);
+                            console.error('Failed to create new object, with error code: ' + error.description);
                         }
                     });
                 }
@@ -48,4 +48,6 @@ AV.Cloud.setInterval('refreash_market', 10, function(){
         });
 
 });
+
+
 
